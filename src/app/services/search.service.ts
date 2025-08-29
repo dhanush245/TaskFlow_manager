8c2d9bd400971067781e10465ca174c436ad4132
dhanush245 <dhanushbabu363@gmail.com>
2025-08-29 12:44:08 +0530

    Fix all errors: Create missing layout components, services, and fix SCSS issues

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private searchTermSubject = new BehaviorSubject<string>('');
+  public searchTerm$ = this.searchTermSubject.asObservable();
+
+  constructor() { }
+
+  setSearchTerm(term: string): void {
+    this.searchTermSubject.next(term);
+  }
+
+  getSearchTerm(): string {
+    return this.searchTermSubject.value;
+  }
+
+  clearSearch(): void {
+    this.searchTermSubject.next('');
+  }
+}
